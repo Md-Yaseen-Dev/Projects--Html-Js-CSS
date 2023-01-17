48c09fffe61858c65054dd16ebc72f6e35fb7ee0
mohammedyaseen2211 <ysn6040@gmail.com>
2023-01-17 11:29:10 +0530

    javascript projects

---
+++ Javascript projects/Calculator/js/calc.js
@@ -0,0 +1,21 @@
+// display(function)
+
+function display(val) {
+    document.getElementById('result').value += val
+    return val;
+}
+
+//solve function
+
+function solve() {
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y
+    return y;
+}
+
+//clearscreen function
+
+function clearscreen(){
+    document.getElementById('result').value ="0"
+}
