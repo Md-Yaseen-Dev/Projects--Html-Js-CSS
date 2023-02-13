b32a057d640df1bd1a2a5d8f5042c7cc78802897
mohammedyaseen2211 <ysn6040@gmail.com>
2023-02-13 23:36:59 +0530

    completed

--- Javascript projects/Clock/js/clock.js
+++ Javascript projects/Clock/js/clock.js
@@ -4,10 +4,11 @@ const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
 setInterval(() => {
     let day = new Date();
-    let hh = day.getHours()*30;
+    let hh = day.getHours() * 30;           // The angle should be 360 degree so that we use 30
     let mm = day.getMinutes() * deg;
     let ss = day.getSeconds() * deg;
     hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
 })
+
